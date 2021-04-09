647a4b29e0a82e20f444e4446dfa969749658399
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2021-04-09 13:17:06 +0300

    Update postfix.h

--- postfix.h
+++ postfix.h
@@ -1,6 +1,7 @@
-// Copyright 2020 A.SHT
+// Copyright 2020 Konina Tatiana
 #ifndef INCLUDE_POSTFIX_H_
 #define INCLUDE_POSTFIX_H_
+
 #include <string>
 
 std::string infix2postfix(std::string infix);
